ac7e64b2ba585142559103759ae66ef00fb47bf2
Prof. José Reginaldo <86785375+profjosereginaldo@users.noreply.github.com>
2024-03-06 13:52:36 +0000

    codigos da aula2 e da pratica1

---
+++ aulas/aula02/variaveis.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int numero;
+  char caracter;
+  int a; // essa variavel eh ler um numero do conficente
+  int x;
+  float nome_composto;
+
+  numero = 10;
+
+  numero = 100;
+  
+  return 0;
+}
